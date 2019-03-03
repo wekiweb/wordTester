e5eca9a1628ca3ed38390f85bf93d69329b95658
wekiweb <wekiweb@naver.com>
2019-03-04 07:37:01 +0900

    Add files via upload

---
+++ wordlist.js
@@ -0,0 +1,10 @@
+var wordlist = new Array();
+wordlist[0] = ["reimbursement", "환급"];
+wordlist[1] = ["premises", "부지, 구내"];
+wordlist[2] = ["impose", "강요하다"];
+wordlist[3] = ["nevertheless", "그럼에도 불구하고"];
+wordlist[4] = ["nonetheless", "그렇더라도"];
+wordlist[5] = ["screening", "심사"];
+wordlist[6] = ["to reach a consensus", "의견일치를 보다"];
+wordlist[7] = ["reluctant to", "~를 꺼리는"];
+wordlist[8] = ["fringe benefits", "복리후생"];
